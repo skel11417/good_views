d44b4240518b6fcb10b17aff4f5929fd6cbb8275
Sean <shonkelli@gmail.com>
2019-04-15 10:42:21 -0400

    fixed migration

--- db/migrate/20190415142418_create_movies.rb
+++ db/migrate/20190415142418_create_movies.rb
@@ -7,7 +7,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.string :director
       t.string :poster
       t.string :actors
-      t.imdb_id :string
+      t.string :imdb_id
       t.response :boolean
       t.timestamps
     end
