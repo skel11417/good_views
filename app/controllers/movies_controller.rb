08dd6d8fcb609b8545d15188461c49dd7b83c89b
Sean <shonkelli@gmail.com>
2019-04-16 11:04:15 -0400

    modified movies controller to allow for multiple search results

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  def index
+  def show
     title = params[:title]
     movie = Movie.find_by(title: title.titleize)
     if movie
@@ -15,6 +15,18 @@ class MoviesController < ApplicationController
       else
         render "not found"
       end
+    end
+  end
+
+  def index
+    title = params[:title]
+    base_url = 'http://www.omdbapi.com/'
+    api_key = Rails.application.credentials.omdb_api_key
+    url = base_url + "?apikey=" + api_key + "&s=" + title + "&type=movie"
+    response = HTTParty.get(url)
+    if response.parsed_response["Response"] == "True"
+      # turn this into something we can use
+      render json: response
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :reviews
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :movies, only: :index
+  resources :movies, only: [:index, :show]
 end
