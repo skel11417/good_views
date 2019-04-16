dd3ad7fa7c65248d0888e9ce1aa4132d0acc8b12
Sean <shonkelli@gmail.com>
2019-04-16 10:04:00 -0400

    added user and review models and migrations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
-
+  has_many :reviews
+  has_many :users, through: :reviews
+  
   def self.create_from_omdb(response)
     r = response.parsed_response
     m = Movie.create(
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_142418) do
+ActiveRecord::Schema.define(version: 2019_04_16_135819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,22 @@ ActiveRecord::Schema.define(version: 2019_04_15_142418) do
     t.string "actors"
     t.string "imdb_id"
     t.boolean "response"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "movie_id"
+    t.string "content"
+    t.integer "rating"
+    t.integer "rank"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
