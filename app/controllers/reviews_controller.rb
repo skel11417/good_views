2964460c1a913bf421bee6077caf46aa54e5e227
seanlucius <seanlucius22@icloud.com>
2019-04-16 12:57:36 -0400

    added some rating functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,12 @@
 class ReviewsController < ApplicationController
+  def create
+    Review.create(review_params)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :movie_id, :rating, :content, :rank)
+  end
+
 end
